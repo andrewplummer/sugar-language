71af62cc7e37ddb771dec2b7c238e49c188281ac
Andrew Plummer <plummer.andrew@gmail.com>
2016-11-05 00:05:33 +0900

    Publish 2.0.2

---
+++ sugar-extended.d.ts
@@ -0,0 +1,36 @@
+// Extended type definitions for Sugar v2.0.2
+// Project: https://sugarjs.com/
+// Definitions by: Andrew Plummer <plummer.andrew@gmail.com>
+
+interface String {
+  hankaku(mode?: string): string;
+  hasArabic(): boolean;
+  hasCyrillic(): boolean;
+  hasDevanagari(): boolean;
+  hasGreek(): boolean;
+  hasHan(): boolean;
+  hasHangul(): boolean;
+  hasHebrew(): boolean;
+  hasHiragana(): boolean;
+  hasKana(): boolean;
+  hasKanji(): boolean;
+  hasKatakana(): boolean;
+  hasLatin(): boolean;
+  hasThai(): boolean;
+  hiragana(all?: boolean): string;
+  isArabic(): boolean;
+  isCyrillic(): boolean;
+  isDevanagari(): boolean;
+  isGreek(): boolean;
+  isHan(): boolean;
+  isHangul(): boolean;
+  isHebrew(): boolean;
+  isHiragana(): boolean;
+  isKana(): boolean;
+  isKanji(): boolean;
+  isKatakana(): boolean;
+  isLatin(): boolean;
+  isThai(): boolean;
+  katakana(): string;
+  zenkaku(mode?: string): string;
+}
